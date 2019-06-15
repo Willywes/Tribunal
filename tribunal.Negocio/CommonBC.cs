f0169a6a0f9910461ca763f78be295feff92ca61
Alejandro Isla <willywes@hotmail.cl>
2019-06-15 02:02:01 -0400

    Agregar archivos de proyecto.

---
+++ tribunal.Negocio/CommonBC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tribunal.DALC;
+
+namespace tribunal.Negocio
+{
+    public class CommonBC
+    {
+        private static LexEntities _lexEntities;
+
+        public static LexEntities LexEntities {
+
+            get {
+
+                if (_lexEntities == null) {
+
+                    _lexEntities = new LexEntities();
+
+                }
+
+                return _lexEntities;
+
+            }
+
+        }
+    }
+}
---
+++ tribunal.Negocio/Comuna.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tribunal.Negocio
+{
+    public class Comuna
+    {
+
+
+        public decimal Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal ProvinciaId { get; set; }
+
+        public Comuna(decimal id, string nombre, decimal provinciaId)
+        {
+            Id = id;
+            Nombre = nombre;
+            ProvinciaId = provinciaId;
+        }
+
+
+        public List<Comuna> All() {
+
+            var comunas =  CommonBC.LexEntities.COMUNA.ToList();
+
+            List<Comuna> lista = new List<Comuna>();
+
+            foreach (var comuna in comunas){
+
+                Comuna c = new Comuna(comuna.ID_COMUNA, comuna.NOMBRE, comuna.ID_PROVINCIA);
+                lista.Add(c);
+
+            }
+
+            return lista;
+
+        }
+       
+    }
+}
